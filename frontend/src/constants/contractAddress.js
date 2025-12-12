6bfd62bcb6e93e57be8f1772aeb97741031b8c08
merins7 <merinsathish7@gmail.com>
2025-12-12 06:04:22 +0000

    Address changed from anvil to sepolia

--- frontend/src/constants/contractAddress.js
+++ frontend/src/constants/contractAddress.js
@@ -1,1 +1,2 @@
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const contractAddress = "0xb47abfa0c5c06859fc434ebc8a591ef75f33e0bd"
+//export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
